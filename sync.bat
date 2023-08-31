bec12bea938a0776121e0a65ee0693fd56baf1a8
lisyoen <lisyoen@gmail.com>
2023-08-31 19:03:55 +0900

    2023-08-31 19:03:55.13 DESKTOP-I7DKUTI

--- sync.bat
+++ sync.bat
@@ -1,16 +1,14 @@
-@echo on
+@echo off
 :LOOP
 echo %DATE% %TIME% Check sync
 git pull
 git add .
 git commit -m "%DATE% %TIME% %USERDOMAIN%
-git status | find "by"
+git status | find "by" > nul
 :PUSH
 echo 변경여부 %ERRORLEVEL%
 if %ERRORLEVEL% == 0 git push
 REM git push timeout 발생시 재시도
-echo push 성공여부 %ERRORLEVEL%
 if %ERRORLEVEL% == 0 goto LOOP
-echo push 실패 1초 후 재시도
 timeout /t 1 > nul
 GOTO PUSH
