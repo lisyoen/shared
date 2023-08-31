4b2e58c3cbabaf3f42d407fbfb2b8203deda1f5f
lisyoen <lisyoen@gmail.com>
2023-08-31 19:17:56 +0900

    2023-08-31 19:17:55.69 DESKTOP-I7DKUTI

--- sync.bat
+++ sync.bat
@@ -15,7 +15,7 @@ REM git push timeout 발생시 재시도
 if %ERRORLEVEL% == 0 (
 	goto LOOP
 ) ELSE (
-	echo push %DATE% %TIME% 실패 1초 후 재시도
+	echo %DATE% %TIME% push 실패 1초 후 재시도
 	timeout /t 1 > nul
 	GOTO PUSH
 )
